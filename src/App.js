93f50f6f948b1c1f5c895112fdb214917b538228
CoffeeJitterz <josh.millard10@gmail.com>
2024-12-09 21:00:37 -0500

    image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import myImage from './images/1.png';  // Import the image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Jarthur
+        {/* Use the image */}
+      <img src={myImage} 
+          alt="Description of the image" />
       </header>
     </div>
   );
